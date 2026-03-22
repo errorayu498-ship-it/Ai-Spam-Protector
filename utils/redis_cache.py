3d6f6469303347c8fa13bf4ed6bf89fa76844055
errorayu498-ship-it <errorayu498@gmail.com>
2026-03-22 14:27:19 +0500

    Update redis_cache.py

--- utils/redis_cache.py
+++ utils/redis_cache.py
@@ -1,1 +1,16 @@
+import redis
+import os
 
+redis_client = redis.from_url(os.getenv("REDIS_URL"))
+
+def set_cache(key, value):
+
+    redis_client.set(key, value)
+
+def get_cache(key):
+
+    return redis_client.get(key)
+
+def delete_cache(key):
+
+    redis_client.delete(key)
