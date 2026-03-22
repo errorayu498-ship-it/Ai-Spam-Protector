0792fa384999318856929e056b41d420eb6aa0d8
errorayu498-ship-it <errorayu498@gmail.com>
2026-03-22 14:27:41 +0500

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,17 @@
+import discord
+from config import CONFIG
 
+async def log_action(bot, guild, message):
+
+    channel = guild.get_channel(CONFIG["log_channel"])
+
+    if not channel:
+        return
+
+    embed = discord.Embed(
+        title="Moderation Log",
+        description=message,
+        color=discord.Color.red()
+    )
+
+    await channel.send(embed=embed)
