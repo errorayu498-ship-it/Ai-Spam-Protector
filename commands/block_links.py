91ac6a7b838e974e4ef4ada1b34caaa03d85934f
errorayu498-ship-it <errorayu498@gmail.com>
2026-03-22 14:26:04 +0500

    Update block_links.py

--- commands/block_links.py
+++ commands/block_links.py
@@ -1,1 +1,25 @@
+import discord
+from discord import app_commands
+import json
 
+class BlockLinks(discord.app_commands.Group):
+
+    def __init__(self):
+        super().__init__(name="block_link", description="Block links")
+
+    @app_commands.command(name="add", description="Block a link")
+    async def add(self, interaction: discord.Interaction, link: str):
+
+        with open("data/links.json", "r") as f:
+            data = json.load(f)
+
+        link_id = str(len(data["blocked"]) + 500)
+
+        data["blocked"][link_id] = link
+
+        with open("data/links.json", "w") as f:
+            json.dump(data, f, indent=4)
+
+        await interaction.response.send_message(
+            f"🚫 Blocked link added\nID: `{link_id}`\nLink: `{link}`"
+        )
