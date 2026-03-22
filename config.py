4019db4dd07857f6c7b6934e88e69640256727da
errorayu498-ship-it <errorayu498@gmail.com>
2026-03-22 14:15:03 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+CONFIG = {
+    "timeout_levels": {
+        1: 3600,      # 1 hour
+        2: 86400,     # 1 day
+        3: 604800     # 1 week
+    },
+
+    "allowed_socials": [
+        "youtube.com",
+        "youtu.be",
+        "tiktok.com",
+        "facebook.com",
+        "instagram.com",
+        "kick.com"
+    ],
+
+    "log_channel": 1377199417274138659
+}
